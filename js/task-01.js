7714de6f2c11a58184d0514d0619666a7aa72bdf
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2022-09-15 09:33:15 +0300

    js-hm-06-02

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesRef = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesRef.length}`)
+
+categoriesRef.forEach(item =>{
+    const titleRef = item.querySelector('h2');
+    const itemsRef = item.querySelectorAll('li')
+    console.log(`Category: ${titleRef.textContent}`)
+    console.log(`Elements: ${itemsRef.length}`)
+
+})
